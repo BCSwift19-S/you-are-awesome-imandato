cabb53c5f6660cc0d079d50e1103b3ffa0f2131d
Ivelisse Mandato <mandatoi@bc.edu>
2019-09-08 15:11:30 -0400

    2.13 completed

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -29,13 +29,24 @@ class ViewController: UIViewController {
                         "You've got the design skills of Jony Ive!",
                         "I can't wait to download your app"]
         
+        var newIndex = -1
+        
+        repeat{
+            newIndex = Int.random(in: 0..<messages.count)
+        } while index == newIndex
+        
+        index = newIndex
         messageLabel.text = messages[index]
         
-        if index == messages.count - 1 {
-            index = 0
-        } else {
-            index = index + 1
-        }
+//        messageLabel.text = messages.randomElement()!
+        
+//        messageLabel.text = messages[index]
+//
+//        if index == messages.count - 1 {
+//            index = 0
+//        } else {
+//            index = index + 1
+//        }
         
 //        let message1 = "You Are Fantastic!!!"
 //        let message2 = "You Are Great!"
